d2c71f6eb62e2784acf356523cfc58efbb7e7c46
drepram <caxvis@gmail.com>
2021-12-20 14:30:21 +0700

    incomplete work on purchasing supplier database due to react hooks

--- pages/role/purchasing/supplier-database.tsx
+++ pages/role/purchasing/supplier-database.tsx
@@ -9,13 +9,17 @@ import LogisticsCategorySearchBar, {
 } from '@components/Logistics/LogisticsCategorySearchBar';
 import { useState } from 'react';
 import { ProjectItemCardProps } from '@components/Project/ProjectItemCard';
+import { GetServerSideProps } from 'next';
+import prisma from '@lib/prisma';
+
+let retrievedData: any;
 
 export default function Page() {
+  const [data, setData] = useState<ProjectItemCardProps>(null);
   const router = useRouter();
   const dummyOnSearch = (data) => {
     console.log(data);
   };
-  const [data, setData] = useState<ProjectItemCardProps>(null);
   return (
     <Layout
       colorType="white"
@@ -39,10 +43,10 @@ export default function Page() {
           <LogisticsCategorySearchBar
             data={DummyCategoryData}
             onSearch={dummyOnSearch}
-            placeholder="Pipe"
           />
         </div>
-        <PurchasingSupplierTable data={DummyData} />
+        <PurchasingSupplierTable data={data} />
+        {/* <PurchasingSupplierTable data={retrievedData.length > 0 ? retrievedData : DummyData} /> */}
       </div>
     </Layout>
   );
@@ -50,42 +54,72 @@ export default function Page() {
 
 const DummyCategoryData: LogisticsCategoryData[] = [
   {
-    categoryName: 'Categ. 1',
-    choices: ['001 barang 1', '001 barang 2', '001 barang 3', '001 barang 4'],
-    categoryId: '1',
-  },
-  {
-    categoryName: 'Categ. 2',
-    choices: ['002 barang 1', '002 barang 2', '002 barang 3', '002 barang 4'],
-    categoryId: '2',
-  },
-  {
-    categoryName: 'Categ. 3',
-    choices: ['003 barang 1', '003 barang 2', '003 barang 3', '003 barang 4'],
-    categoryId: '3',
-  },
-  {
-    categoryName: 'Categ. 4',
-    choices: ['004 barang 1', '004 barang 2', '004 barang 3', '004 barang 4'],
-    categoryId: '4',
-  },
+    'categoryName': 'PIPE',
+    'choices': ['Pipe Seamless 40A C.Steel Sch.40','Pipe Seamless 80A C.Steel Sch.40','Pipe Seamless 150A C.Steel Sch.40','Pipe Seamless 200A C.Steel Sch.40','Pipe Seamless 250A C.Steel Sch.40'],
+    'categoryId': '1'
+    },
+    {
+    'categoryName': 'FITTING',
+    'choices': ['Reducer  150A/80A C.Steel Sch.40','Reducer  200A/150A C.Steel Sch.40','Reducer  250A/150A C.Steel Sch.40','Gasket 40A Rubber JIS 5K','Gasket 80A Rubber JIS 5K','Gasket 200A Rubber JIS 5K','Gasket 80A Rubber JIS 10K','Gasket 150A Rubber JIS 10K','Gasket 200A Rubber JIS 10K','Gasket 250A Rubber JIS 10K','Gasket 80A Rubber JIS 16K','Gasket 200A Rubber JIS 16K','Flange 40A Mild Steel JIS 5K Slip On','Flange 80A Mild Steel JIS 5K Slip On','Flange 200A Mild Steel JIS 5K Slip On','Flange 80A Mild Steel JIS 10K Slip On','Flange 150A Mild Steel JIS 10K Slip On','Flange 200A Mild Steel JIS 10K Slip On','Flange 250A Mild Steel JIS 10K Slip On','Flange 80A Mild Steel JIS 16K Slip On','Flange 200A Mild Steel JIS 16K Slip On','Plate Penetration 40A Mild Steel JIS 10K','Plate Penetration 150A Mild Steel JIS 10K','Plate Penetration 200A Mild Steel JIS 10K','Plate Penetration 250A Mild Steel JIS 10K','Elbow 150A C.Steel Sch.40 30 Deg.','Elbow 200A C.Steel Sch.40 30 Deg.','Elbow 80A C.Steel Sch.40 45 Deg.','Elbow 150A C.Steel Sch.40 45 Deg.','Elbow 200A C.Steel Sch.40 45 Deg.','Elbow 80A C.Steel Sch.40 90 Deg.','Elbow 150A C.Steel Sch.40 90 Deg.','Elbow 200A C.Steel Sch.40 90 Deg.','Elbow 250A C.Steel Sch.40 90 Deg.','Sounding Cap 40A Bronze','Vent Head 200A Mild Steel Galv. JIS 5K'],
+    'categoryId': '2'
+    },
+    {
+    'categoryName': 'VALVES',
+    'choices': ['Butterfly Valve 150A Cast Iron JIS 10K Flange Type','Butterfly Valve 250A Cast Iron JIS 10K Water Type','Check Valve 150A Cast Iron JIS 10K Swing Type','Check Valve 250A Cast Iron JIS 10K Swing Type'],
+    'categoryId': '3'
+    }
 ];
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const items = await prisma.item.findMany({
+    where: {
+      name: 'Pipe Seamless 40A C.Steel Sch.40'
+    },
+    select: {
+      id: true,
+      name: true,
+      ItemsOnSuppliers: {
+        select: {
+          supplier: {
+            select: {
+              name: true,
+              country: true
+            }
+          }
+        }
+      },
+    },
+  });
+
+  const retrievedData: PurchasingSupplierData[] = items.map((log) => ({
+    itemType: log.id,
+    itemName: log.name+'',
+    supplier: log.ItemsOnSuppliers[0].supplier.name+'',
+    location: log.ItemsOnSuppliers[0].supplier.country+''
+  }));
+
+  return {
+    props: {
+      retrievedData,
+    },
+  };
+}
 
 const DummyData: PurchasingSupplierData[] = [
   {
-    itemType: '1',
+    itemType: 1,
     itemName: 'kucing',
     supplier: 'google',
     location: 'jakarta',
   },
   {
-    itemType: '1',
+    itemType: 2,
     itemName: 'kucing',
     supplier: 'google',
     location: 'jakarta',
   },
   {
-    itemType: '1',
+    itemType: 3,
     itemName: 'kucing',
     supplier: 'google',
     location: 'jakarta',
