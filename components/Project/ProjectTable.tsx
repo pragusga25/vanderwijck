2f615ee8cd42380213cedf7d26ebca5bbb489e51
zidan <zidan.kharisma@ui.ac.id>
2021-12-23 09:26:02 +0700

    add time & requestedBy field on project/status

--- components/Project/ProjectTable.tsx
+++ components/Project/ProjectTable.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 export interface ProjectStatusData {
+  date?: string;
   projectNo: string;
   transactionNumber: string;
   itemName: string;
   subcode: string;
   qty: string;
   unit: string;
+  requestedBy?:string;
   approvedBy: string;
   status: string;
 }
@@ -15,26 +17,30 @@ const StatusTable: React.FC<{ data: ProjectStatusData[] }> = ({ data }) => {
       <table>
         <thead className="text-sm">
           <tr>
-            <th className="w-1/12">Project No</th>
-            <th className="w-2/12">Transaction Number</th>
-            <th className="w-4/12">Item Name</th>
-            <th className="w-1/12">Subcode</th>
-            <th className="w-1/12">Qty</th>
-            <th className="w-1/12">Unit</th>
-            <th className="w-2/12">Approved By</th>
-            <th className="w-1/12">Status</th>
+            <th style={{ width: '12%' }}>Waktu</th>
+            <th style={{ width: '10%' }}>Project No</th>
+            <th style={{ width: '10%' }}>Transaction Number</th>
+            <th style={{ width: '20%' }}>Item Name</th>
+            <th style={{ width: '5%' }}>Subcode</th>
+            <th style={{ width: '5%' }}>Qty</th>
+            <th style={{ width: '5%' }}>Unit</th>
+            <th style={{ width: '12%' }}>Requested By</th>
+            <th style={{ width: '12%' }}>Approved By</th>
+            <th style={{ width: '8%' }}>Status</th>
           </tr>
         </thead>
         <tbody>
           {data.map((e) => {
             return (
               <tr>
+                <td className="border-black border">{e.date ?? ''}</td>
                 <td className="border-black border">{e.projectNo}</td>
                 <td className="border-black border">{e.transactionNumber}</td>
                 <td className="border-black border">{e.itemName}</td>
                 <td className="border-black border">{e.subcode}</td>
                 <td className="border-black border">{e.qty}</td>
                 <td className="border-black border">{e.unit}</td>
+                <td className="border-black border">{e.requestedBy ?? ''}</td>
                 <td className="border-black border">{e.approvedBy}</td>
                 <td className="border-black border">{e.status}</td>
               </tr>
