5fd94c596d319225fb66610e02b6e324467e18d5
drepram <caxvis@gmail.com>
2021-12-26 21:23:16 +0700

    swap condition

--- prisma/seedItemsOfSuppliers.ts
+++ prisma/seedItemsOfSuppliers.ts
@@ -2,26 +2,26 @@
 
 import {
     items,
-    suppliers,
+    locations,
   } from '../data';
   import prisma from '../lib/prisma';
   
   // const prisma = new PrismaClient();
   
   const seed = async () => {  
-    await prisma.supplier.createMany({ data: suppliers });
-    for (const supplier of suppliers) {
-      for (const item of items) {
-        if ((supplier.category as string[]).includes(item.category)) {
-          await prisma.itemsOnSuppliers.create({
-            data: {
-              supplier: { connect: { id: supplier.id } },
-              item: { connect: { code: item.code } },
-            },
-          });
-        }
-      }
-    }
+    await prisma.location.createMany({ data: locations });
+    // for (const supplier of suppliers) {
+    //   for (const item of items) {
+    //     if ((supplier.category as string[]).includes(item.category)) {
+    //       await prisma.itemsOnSuppliers.create({
+    //         data: {
+    //           supplier: { connect: { id: supplier.id } },
+    //           item: { connect: { code: item.code } },
+    //         },
+    //       });
+    //     }
+    //   }
+    // }
   };
   
   seed()
